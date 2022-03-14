b77b5258dc362a0858aa2ae024c68ca933f19798
James Mwendwa <mwendwajames32@gmail.com>
2022-03-14 15:13:34 +0300

    files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { MdOutlineDoubleArrow } from 'react-icons/md';
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h2>Experience <MdOutlineDoubleArrow /> </h2>
+      
   );
 }
 
